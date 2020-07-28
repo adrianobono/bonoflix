ab099edd47a5dd083d3dce6ce987547c85f21e40
adriano bono <adriano@radek.com>
2020-07-28 12:54:27 -0300

    step5

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"Com Javascript + Html + Css você pode criar este jogo"}
+        videoDescription={"Javascript + Html + Css você pode criar este jogo"}
       />
 
       <Carousel
