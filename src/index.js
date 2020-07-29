1628c98956fa95ee9fa0e38ac4bfd80ab45bf4dc
adriano bono <adriano@radek.com>
2020-07-28 22:37:44 -0300

    Stage6

--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video'
+import CadastroCategoria from './pages/cadastro/categoria';
+ 
+// function CadastroVideo() {
+//   return (
+//     <div>
+//       Página de Cadastro de Vídeo.
+//     </div>
+//   )
+// }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+    <Route path="/" component={App} exact />
+    <Route path="/cadastro/video" component={CadastroVideo} />
+    <Route path="/cadastro/categoria" component={CadastroCategoria} />
+    <Route  component={() => (<div>Página 404</div>)} />
+  </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
