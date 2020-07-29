f7678f33cfc30322fab6019d11f16e5e679b52de
adriano bono <adriano@radek.com>
2020-07-28 23:11:31 -0300

    stage9

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
-import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+
+function CadastroCategoria() {
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
+
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
 
 
-function CadastroCategoria () {
-    return (
-        <PageDefault>
-            <h1>Página de Cadastro de Categoria.</h1>
-        
-        <Link to ="/">
-           Ir para home
-        </Link>
-
-        </PageDefault>
-
-    )
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
 }
 
 export default CadastroCategoria;
