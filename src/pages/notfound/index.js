63b02ca6edd528e89561d32ecc2905d57ded2781
adriano bono <adriano@radek.com>
2020-07-31 08:34:09 -0300

    removegame

--- src/pages/notfound/index.js
+++ src/pages/notfound/index.js
@@ -5,13 +5,13 @@ import { ResponsiveIframe } from './styles';
 
 function NotFound() {
   return (
-
-     <PageDefault>
-        <ResponsiveIframe
-          title="Titulo do Iframe"
-          src={`https://www.icanvas.com.br/flappy`}
-          />
-      </PageDefault>
+<div></div>
+    //  <PageDefault>
+    //     <ResponsiveIframe
+    //       title="Titulo do Iframe"
+    //       src={`https://www.icanvas.com.br/flappy`}
+    //       />
+    //   </PageDefault>
  
   );
 }
